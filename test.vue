61f8a1d2e2f16646a4b3c2281eca84175c32cd02
umaaneja <102696553+umaaneja@users.noreply.github.com>
2025-04-30 06:52:40 +0100

    Update test.vue

--- test.vue
+++ test.vue
@@ -1,6 +1,30 @@
+from docx import Document
+from docx.oxml import OxmlElement
+from docx.oxml.ns import qn
 
-<table style="border-collapse: collapse; border: 1pt solid black;">
-  <tr>
-    <td style="border: 1pt solid black; padding: 4pt;">Content</td>
-  </tr>
-</table>
+def add_table_borders(docx_file, output_file):
+    """Add borders to all tables in a DOCX file"""
+    doc = Document(docx_file)
+    
+    # Function to set cell borders
+    def set_cell_borders(cell):
+        tc = cell._tc
+        tcPr = tc.get_or_add_tcPr()
+        
+        # Create border elements
+        for border in ['top', 'left', 'bottom', 'right']:
+            tag = f'w:{border}'
+            element = OxmlElement(tag)
+            element.set(qn('w:val'), 'single')
+            element.set(qn('w:sz'), '4')  # 0.5pt border
+            element.set(qn('w:space'), '0')
+            element.set(qn('w:color'), '000000')  # Black
+            tcPr.append(element)
+    
+    # Process all tables
+    for table in doc.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                set_cell_borders(cell)
+    
+    doc.save(output_file)
